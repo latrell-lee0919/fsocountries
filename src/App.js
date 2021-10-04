508c6098337c285da4d50934d7662cdd4429800a
Latrell <lll4xy@virginia.edu>
2021-10-03 22:59:58 -0400

    starting exercise 2.12

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function App() {
+const Filter = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries <input value={props.newFilter} onChange={props.handleFilterChange}/>
     </div>
-  );
+  )
+}
+
+const Countries = (props) => {
+  console.log(props.count)
+  if(props.count === 1) {
+    return (
+      <div>
+        <h1>{props.name.common}</h1>
+        <div>capital {props.capital[0]}</div>
+        <div>population {props.population}</div>
+        <h2>languages</h2>
+        {props.languages.map(language => {
+          <ul>{language}</ul>
+        })}
+        {props.flag}
+      </div>
+    )
+  } else if (props.count <= 10) {
+    return (
+      <div>
+        {props.name.common}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        Too many matches, specify another filter
+      </div>
+    )
+  }
+}
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [newFilter, setNewFilter] = useState('');
+
+  const handleFilterChange = (event) => {
+    setNewFilter(event.target.value);
+  }
+
+  useEffect(() => {
+    axios.get('https://restcountries.com/v3.1/all')
+    .then(response => {
+      console.log(response.data)
+      setCountries(response.data)
+    })
+  }, [])
+
+  return (
+    <div>
+      <Filter newFilter={newFilter} handleFilterChange={handleFilterChange}/>
+      {countries.filter(country => country.name.common.toUpperCase()
+      .includes(newFilter.toUpperCase()))
+      .map(country => 
+      <Countries key={country.area} name={country.name}/>)}
+    </div>
+  )
 }
 
 export default App;
